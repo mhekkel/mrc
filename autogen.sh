321b7f547bd56e4a417b59f17cd99c17c059ca43
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-06-02 14:58:20 +0200

    added autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eu
+
+aclocal
+
+wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+
+autoconf
+
+echo "done"
