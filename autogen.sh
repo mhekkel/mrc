55de491f4947b23428f4254c0966025fe0c644cf
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-06-02 17:08:08 +0200

    quiet autogen

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,8 @@ set -eu
 
 aclocal
 
-wget -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
-wget -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+wget --quiet -O config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+wget --quiet -O config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 
 autoconf
 
